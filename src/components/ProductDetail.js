3e089d1bdd90e779a411d715af77a479c1778f6a
Bharti277 <bhartideependra277@gmail.com>
2023-10-09 17:08:35 +0530

    product detail wip

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { selectedProducts } from "../redux/dashboardAction";
 
 function ProductDetail() {
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+  console.log(productId, "product id");
+  const fetchProductDetail = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => console.log(err));
+    dispatch(selectedProducts(response.data));
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") {
+      fetchProductDetail();
+    }
+  }, [productId]);
+
   return <div>ProductDetail</div>;
 }
 
--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { setProducts } from "../redux/dashboardAction";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ProductListing = () => {
   const products = useSelector((state) => state.allProducts.products);
@@ -22,17 +23,19 @@ const ProductListing = () => {
       {products?.map((prod) => {
         return (
           <div key={prod.id} className="p-8 border-4">
-            <div className="">
-              <h2 className="h-12">{prod.title}</h2>
-              <div className="inline-block border-2 border-gray-500 w-52">
-                <img src={prod.image} alt="" className="w-52 h-52" />
+            <Link to={`/product/${prod.id}`}>
+              <div className="">
+                <h2 className="h-12">{prod.title}</h2>
+                <div className="inline-block border-2 border-gray-500 w-52">
+                  <img src={prod.image} alt="" className="w-52 h-52" />
+                </div>
+                <p>${prod.price}</p>
+                <p>{prod.description}</p>
               </div>
-              <p>${prod.price}</p>
-              <p>{prod.description}</p>
-            </div>
-            <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">
-              Add to cart
-            </button>
+              <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700">
+                Add to cart
+              </button>
+            </Link>
           </div>
         );
       })}
