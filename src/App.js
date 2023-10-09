bc7dd88e59f8d9c3f036bb81f9b7e511588f8416
Bharti277 <bhartideependra277@gmail.com>
2023-10-09 16:31:14 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import ProductListing from "./components/ProductListing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
 function App() {
   return (
     <div className="box-border p-0 m-0">
       <Navbar />
-      <div className="">
-        <ProductListing />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact Component={ProductListing} />
+          <Route path="/product/:productId" exact Component={ProductDetail} />
+          <Route>404 found!</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
